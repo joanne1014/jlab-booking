f566bea6d0be8369d2d82989bce68ffc7f96da81
joanne1014 <waiwaib1014@gmail.com>
2026-04-22 17:33:19 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Admin from './Admin';
+
+const path = window.location.pathname;
+const isAdmin = path.startsWith('/admin');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {isAdmin ? <Admin /> : <App />}
+  </React.StrictMode>
+);
