edb399b3bca816af871afa2419f0a3d732c7db7d
joanne1014 <waiwaib1014@gmail.com>
2026-05-02 02:38:46 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing.jsx'
-import AboutPage from './pages/AboutPage.jsx'
-import BookingPage from './pages/BookingPage.jsx'
-import Admin from './Admin.jsx'
-import ResetPassword from './ResetPassword.jsx'
+import Landing from './pages/Landing'
+import BookingPage from './pages/BookingPage'
+import AboutPage from './pages/AboutPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<AboutPage />} />
         <Route path="/booking" element={<BookingPage />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
