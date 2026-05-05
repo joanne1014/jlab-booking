4c8d5522c13a7036e8f05f4d3acc309a72b39771
joanne1014 <waiwaib1014@gmail.com>
2026-05-05 14:22:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
-import { Navigate } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import BookingPage from './pages/BookingPage';
+import Landing from './pages/Landing';
+import AboutPage from './pages/AboutPage';
+import Admin from './Admin';
+import ResetPassword from './ResetPassword';
+import ReceiptsPage from './pages/ReceiptsPage';
+
 export default function App() {
-  return <Navigate to="/" replace />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 客戶頁面 */}
+        <Route path="/" element={<BookingPage />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/about" element={<AboutPage />} />
+
+        {/* 管理員頁面 */}
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/receipts" element={<ReceiptsPage />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+
+        {/* 其他 URL 全部去首頁 */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
