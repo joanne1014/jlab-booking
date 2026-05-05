ba20000872a05321f4a0cb0dd0a48f1d8dcce088
joanne1014 <waiwaib1014@gmail.com>
2026-05-05 23:51:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  },
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          supabase: ['@supabase/supabase-js'],
+          charts: ['recharts']
         }
       }
     }
   }
-})
+});
